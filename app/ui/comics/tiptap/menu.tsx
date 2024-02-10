4a2017a41a0e16d278ba748854462d38390ab056
Tyler Pohl <tylercurtispohl@gmail.com>
2024-02-09 16:18:21 -0800

    button to output more info about the anchor position and trying to insert dialog before current node

--- app/ui/comics/tiptap/menu.tsx
+++ app/ui/comics/tiptap/menu.tsx
@@ -3,6 +3,8 @@
 import { Editor } from "@tiptap/react";
 import { Button } from "@nextui-org/react";
 import {
+  ArrowDownIcon,
+  ArrowUpIcon,
   ChatBubbleLeftIcon,
   DocumentIcon,
   SquaresPlusIcon,
@@ -11,22 +13,41 @@ import React from "react";
 import { useCustomCommands } from "./useCustomCommands";
 
 export const ComicEditorMenu = ({ editor }: { editor: Editor }) => {
-  const { insertPanel, insertDialog, insertPage, logJson } =
-    useCustomCommands(editor);
+  const {
+    insertPanel,
+    insertDialog,
+    insertPage,
+    logJson,
+    logAnchorPos,
+    insertDialogBefore,
+  } = useCustomCommands(editor);
 
   return (
     <div className="flex flex-row justify-start gap-2 py-2">
-      <Button color="default" size="sm" variant="light" onClick={insertPage}>
+      <Button color="default" size="sm" variant="ghost" onClick={insertPage}>
         <DocumentIcon className="h-6 w-6" />
       </Button>
-      <Button color="default" size="sm" variant="light" onClick={insertPanel}>
+      <Button color="default" size="sm" variant="ghost" onClick={insertPanel}>
         <SquaresPlusIcon className="h-6 w-6" />
       </Button>
-      <Button color="default" size="sm" variant="light" onClick={insertDialog}>
+      <Button
+        color="default"
+        size="sm"
+        variant="ghost"
+        onClick={insertDialogBefore}
+      >
         <ChatBubbleLeftIcon className="h-6 w-6" />
+        <ArrowUpIcon className="h-4 w-4" />
       </Button>
-      <Button color="default" size="sm" variant="light" onClick={logJson}>
+      <Button color="default" size="sm" variant="ghost" onClick={insertDialog}>
+        <ChatBubbleLeftIcon className="h-6 w-6" />
+        <ArrowDownIcon className="h-4 w-4" />
+      </Button>
+      <Button color="default" size="sm" variant="ghost" onClick={logJson}>
         Log JSON
+      </Button>
+      <Button color="default" size="sm" variant="ghost" onClick={logAnchorPos}>
+        Log AnchorPos
       </Button>
     </div>
   );
--- app/ui/comics/tiptap/useCustomCommands.ts
+++ app/ui/comics/tiptap/useCustomCommands.ts
@@ -1,11 +1,13 @@
-import { Editor } from "@tiptap/react";
+import { Editor, NodePos } from "@tiptap/react";
 
 export const useCustomCommands = (editor: Editor) => {
   return {
     insertPage: () => insertPage(editor),
     insertPanel: () => insertPanel(editor),
     insertDialog: () => insertDialog(editor),
+    insertDialogBefore: () => insertDialogBefore(editor),
     logJson: () => logJson(editor),
+    logAnchorPos: () => logAnchorPos(editor),
   };
 };
 
@@ -21,8 +23,17 @@ const insertPanel = (editor: Editor) =>
     .insertContent("<h2>Panel X</h2>")
     .run();
 
-const insertDialog = (editor: Editor) =>
-  editor
+const insertDialog = (editor: Editor) => {
+  const anchorPos = editor.$pos(editor.state.selection.anchor);
+  console.log("anchor pos");
+  console.log(anchorPos);
+  // const nodePos = new NodePos(anchorPos, editor);
+  const closestDialog = anchorPos.closest("paneldialog");
+  console.log("closest paneldialog pos");
+  console.log(closestDialog);
+  console.log(closestDialog?.parent);
+
+  return editor
     .chain()
     .focus()
     .enter()
@@ -42,5 +53,68 @@ const insertDialog = (editor: Editor) =>
     .selectNodeBackward()
     .selectTextblockStart()
     .run();
+};
+
+const insertDialogBefore = (editor: Editor) => {
+  const anchorPos = editor.$pos(editor.state.selection.anchor);
+  const currentNode = anchorPos.node;
+  // if current pos is in panel dialog (either character name or dialog)
+  //    insert content before closest charactername
+  // if current pos is in panel heading or description
+  //    if there is a previous panel
+  //        insert content at end of previous panel
+  // if current pos is in page heading or page description
+  //    if there is a previous page
+  //        insert content at end of previous page
+  let pos: NodePos | null | undefined;
+  if (currentNode.type.name === "charactername") {
+    pos = anchorPos.before;
+  }
+
+  if (!pos) {
+    return false;
+  }
+
+  return editor
+    .chain()
+    .focus()
+    .insertContentAt(anchorPos, [
+      {
+        type: "charactername",
+        attrs: {
+          level: 3,
+        },
+        content: [
+          {
+            type: "text",
+            text: "Character Name",
+          },
+        ],
+      },
+      {
+        type: "dialog",
+        content: [
+          {
+            type: "text",
+            text: "Dialog",
+          },
+        ],
+      },
+    ]);
+};
 
 const logJson = (editor: Editor) => console.log(editor.getJSON());
+
+const logAnchorPos = (editor: Editor) => {
+  const anchorPos = editor.$pos(editor.state.selection.anchor);
+  console.log("anchorPos");
+  console.log(anchorPos);
+  console.log("before");
+  console.log(anchorPos.before);
+  console.log("node");
+  console.log(anchorPos.node);
+  console.log("content");
+  console.log(anchorPos.content);
+  console.log("lastChild");
+  console.log(anchorPos.lastChild);
+};
